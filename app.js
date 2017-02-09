6048af1dbec3dde4141494659d7a79e59f5d4895
Melinda Yeoh OG <melindayeoh OG@melinda-yeohs-macbook.local>
2017-02-09 17:58:18 -0500

    Added basic functionality to photo browser. No CSS yet.

--- app.js
+++ app.js
@@ -2,34 +2,35 @@ var FLICKR_API_URL = 'https://api.flickr.com/services/rest/?method=flickr.photos
 var FLICKR_API_KEY = '9c0b191a1d8415714a70a2a3db4abdeb';
 
 
-getPhotosForSearch(word){
+function getPhotosForSearch(word) {
 
-	var url = `${FLICKR_API_URL}${FLICKR_API_KEY}&extras=url_q,url_s&text=${word}`;
+    //var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=9c0b191a1d8415714a70a2a3db4abdeb&text=' + word;
+    var url = `${FLICKR_API_URL}${FLICKR_API_KEY}&extras=url_q,url_s&text=${word}`;
 
-	console.log("flickr url: ", url);
+    // console.log("flickr url: ", url);
 
-	return (
-		fetch(url)
-		.then(response => response.json())
-		.then(data => data.photos.photo) //will return the array of photos (each photo being objects)
-		);
+    return (
+        fetch(url)
+        .then(response => response.json())
+        .then(data => data.photos.photo) //will return the array of photos (each photo being objects)
+    ); //fetch request working
 };
 
 function createFlickrThumb(photoData) {
 
-	console.log("data sent to linkify: ", photoData);
+    // console.log("data sent to linkify: ", photoData);
 
-	var link = document.createElement('a');
-	link.setAttribute('href', photoData.url_q);
-	link.setAttribute('target', '_blank');
+    var link = document.createElement('a');
+    link.setAttribute('href', photoData.url_q);
+    link.setAttribute('target', '_blank');
 
-	var image = document.createElement('img');
-	image.setAttribute('src', photoData.url_s);
-	image.setAttribute('alt', photoData.title);
+    var image = document.createElement('img');
+    image.setAttribute('src', photoData.url_s);
+    image.setAttribute('alt', photoData.title);
 
-	link.appendChild(image);
+    link.appendChild(image);
 
-	return link;
+    return link;
 }
 
 //=================================WIRING TO THE DOM===============================================
@@ -37,30 +38,40 @@ var app = document.querySelector('#app');
 var searchForm = app.querySelector('.search-form');
 var wordInput = searchForm.querySelector('.search-input');
 var getPhotosButton = searchForm.querySelector('.get-photos-button');
-var results = app.querySelector('.results');
+var results = app.querySelector('#results'); //by making the results an ID versus CLASS, it's more specific and you're able to refer to it as the parent Node versus classes which are more general.
 
 
 
 //eventhandlers
-searchForm.addEventListener('submit', function(event){
-	event.preventDefault();
+searchForm.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-	var word = wordInput.value; 
+    var word = wordInput.value;
 
-	console.log("word entered: ", word);
+    // console.log("word entered: ", word);
+    while (results.hasChildNodes()) {
+        results.removeChild(results.lastChild);
+    }
 
-	results.innerText = "loading...";
+    results.innerText = "loading...";
 
-	getPhotosForSearch(word)
-	.then(function(response){
-		console.log("results: ", response, typeof response);
+    getPhotosForSearch(word)
+        .then(function(response) {
+            // console.log("photos: ", response, typeof response);
 
-		results.innerText = "";
+            results.innerText = "";
 
-		var links = response.map(createFlickrThumb);
+            // var link;
+            // response.forEach(function(eachPhoto){
+            // 	link = createFlickrThumb(eachPhoto);
+            // 	app.appendChild(link);
+            // })
+            var links = response.map(createFlickrThumb);
 
-		results.appendChild(links);
-	});
+            links.forEach(function(link) {
+                results.appendChild(link);
+            });
+        });
 
 
 });
